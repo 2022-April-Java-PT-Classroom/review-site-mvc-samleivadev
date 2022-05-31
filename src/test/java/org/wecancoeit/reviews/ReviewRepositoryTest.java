41f5be588095c266799be15ca5209918f65dfe87
samleivadev <samdleiva@gmail.com>
2022-05-31 13:56:16 -0400

    reviewControllerMvcTest, all test passing 7 of 7

--- src/test/java/org/wecancoeit/reviews/ReviewRepositoryTest.java
+++ src/test/java/org/wecancoeit/reviews/ReviewRepositoryTest.java
@@ -2,6 +2,9 @@ package org.wecancoeit.reviews;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Collection;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -13,11 +16,16 @@ public class ReviewRepositoryTest {
     @Test
     public void shouldFindOneReview(){
 
-        ReviewRepository underTest = new ReviewRepository (reviewOne);
+        underTest = new ReviewRepository (reviewOne);
         Review foundReview = underTest.findOne(1L);
         assertEquals (reviewOne, foundReview);
+    }
 
-
+    @Test
+    public void shouldFindTwoReviews(){
+        underTest = new ReviewRepository(reviewOne,reviewTwo);
+        Collection<Review> foundReviews = underTest.findAll();
+        assertThat(foundReviews).contains(reviewOne,reviewTwo);
 
     }
 }
