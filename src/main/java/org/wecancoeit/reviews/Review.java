b7c79aeac37f2bf4b927420e45f533f64a08d515
samleivadev <samdleiva@gmail.com>
2022-06-07 19:09:37 -0400

    css for review at 50%

--- src/main/java/org/wecancoeit/reviews/Review.java
+++ src/main/java/org/wecancoeit/reviews/Review.java
@@ -5,12 +5,21 @@ public class Review {
     private String team;
     private String image;
     private String review;
+    private String description;
+    private String country;
+    private String imgTeam;
+    private String coach;
 
-    public Review (Long id, String team, String image, String review) {
+
+    public Review (Long id, String team, String image, String review, String description, String country, String imgTeam, String coach) {
         this.id = id;
         this.team = team;
         this.image = image;
         this.review = review;
+        this.description=description;
+        this.country=country;
+        this.imgTeam=imgTeam;
+        this.coach=coach;
     }
 
     public Long getId() {
@@ -27,5 +36,21 @@ public class Review {
 
     public String getReview() {
         return review;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getImgTeam() {
+        return imgTeam;
+    }
+
+    public String getCoach() {
+        return coach;
     }
 }
--- src/main/java/org/wecancoeit/reviews/ReviewRepository.java
+++ src/main/java/org/wecancoeit/reviews/ReviewRepository.java
@@ -11,11 +11,11 @@ public class ReviewRepository {
     Map<Long, Review> reviewslist = new HashMap<>();
 
     public ReviewRepository(){
-        Review realMadridFc = new Review(1L, "Real Madrid FC", "/images/realmadrid.png","5 stars");
-        Review barcelonaFc = new Review(2L, "Barcelona FC", "/images/barcelona.png","4.9 stars");
-        Review manchesterUnited = new Review(3L, "Manchester United", "/images/munited.png", "4.89 stars");
-        Review juventus = new Review(4L, "Juventus", "/images/juventus.png","4.5 stars");
-        Review chelsea = new Review(5L, "Chelsea FC", "/images/chelsea.png","4 stars");
+        Review realMadridFc = new Review(1L, "Real Madrid FC", "/images/realmadrid.png","5 stars","Hala Madrid! Best team in the world 2022","Spain","/images/RMTeam.jpg","Carlo Ancelotti");
+        Review barcelonaFc = new Review(2L, "Barcelona FC", "/images/barcelona.png","4.9 stars","Hala Madrid! Best team in the world 2022","Spain","/images/baceTeam.jpg","Xavi Hernández");
+        Review manchesterUnited = new Review(3L, "Manchester United", "/images/munited.png", "4.89 stars","Hala Madrid! Best team in the world 2022","England","/images/manTeam.jpg","Erik ten Hag");
+        Review juventus = new Review(4L, "Juventus", "/images/juventus.png","4.5 stars","Hala Madrid! Best team in the world 2022","Italy","/images/juveTeam.jpg","Joe Montemurro");
+        Review chelsea = new Review(5L, "Chelsea FC", "/images/chelsea.png","4 stars","Hala Madrid! Best team in the world 2022","England","/images/chelTeam.jpg","Thomas Tuchel");
 
         reviewslist.put(realMadridFc.getId(),realMadridFc);
         reviewslist.put(barcelonaFc.getId(),barcelonaFc);
